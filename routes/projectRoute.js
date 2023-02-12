4762baba6df89ec6cb017ecc6c81f1b7e0e2ce3f
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-12 23:20:55 +0600

    routes added

---
+++ routes/projectRoute.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+// router
+const router = express.Router();
+
+// GET all projects
+router.get("/", (req, res) => {
+  res.json({ message: "GET all projects" });
+});
+
+// GET a single project
+router.get("/:id", (req, res) => {
+  res.json({ message: "GET a single project" });
+});
+
+// POST a new project
+router.post("/", (req, res) => {
+  res.json({ message: "POST a new project" });
+});
+
+// DELETE a project
+router.delete("/:id", (req, res) => {
+  res.json({ message: "DELETE a project" });
+});
+
+// UPDATE a project
+router.patch("/:id", (req, res) => {
+  res.json({ message: "PATCH a project" });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
+const projectRoutes = require("./routes/projectRoute");
 
 // express app
 const app = express();
 
 // port
 const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/projects", projectRoutes);
 
 // listen for requests
 app.listen(port, () => {
